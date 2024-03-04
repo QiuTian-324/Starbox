1038302c0a9b85034f88737b95d0f0bd03138ddd
QiuTian-324 <1240092443@qq.com>
2024-03-04 23:41:24 +0800

    :wrench:config:初始化Sms、BizRedis、UserRpc

--- service/applet/internal/config/config.go
+++ service/applet/internal/config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "github.com/zeromicro/go-zero/rest"
+import (
+	"github.com/zeromicro/go-zero/core/stores/redis"
+	"github.com/zeromicro/go-zero/rest"
+	"github.com/zeromicro/go-zero/zrpc"
+)
 
 type Config struct {
 	rest.RestConf
@@ -8,4 +12,13 @@ type Config struct {
 		AccessSecret string
 		AccessExpire int64
 	}
+	Sms struct {
+		AccessKeyId     string
+		AccessKeySecret string
+		SignName        string
+		TemplateCode    string
+		Endpoint        string
+	}
+	BizRedis redis.RedisConf
+	UserRPC  zrpc.RpcClientConf
 }
